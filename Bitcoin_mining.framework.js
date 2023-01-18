1f4f884803f00d5d75c7d1f3b59ab1be43fc341c
Mylo Cyrus <irahiggins740@gmail.com>
2023-01-18 15:53:12 -0800

    Create Bitcoin_mining.framework.js

---
+++ Bitcoin_mining.framework.js
@@ -0,0 +1,15 @@
+Request URL: https://gamebol.com/gamesource/Bitcoin_Mining/Build/Bitcoin_Mining.framework.js
+Request Method: GET
+Status Code: 200  (from disk cache)
+Remote Address: [2607:7700:0:38:0:1:951c:e7a2]:443
+Referrer Policy: strict-origin-when-cross-origin
+accept-ranges: bytes
+cache-control: max-age=604800
+content-encoding: br
+content-length: 75514
+content-type: application/javascript
+date: Wed, 18 Jan 2023 21:58:54 GMT
+expires: Wed, 25 Jan 2023 21:58:54 GMT
+last-modified: Tue, 28 Jun 2022 12:02:22 GMT
+server: Apache
+vary: Accept-Encoding

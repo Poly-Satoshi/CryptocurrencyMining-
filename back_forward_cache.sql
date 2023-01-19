43313ef89afad4d285f7c88bf10d4f29f9f950b9
Mylo Cyrus <irahiggins740@gmail.com>
2023-01-18 16:14:47 -0800

    Add files via upload

---
+++ back_forward_cache.sql
@@ -0,0 +1,25 @@
+IndexedDB is not available. Data will not persist in cache and PlayerPrefs will not be saved.
+Bitcoin_Mining.framework.js:2 Loading player data from data.unity3d
+
+Bitcoin_Mining.framework.js:2 Initialize engine version: 2021.1.11f1 (430e224e4fe3)
+
+Bitcoin_Mining.loader.js:1 Creating WebGL 2.0 context.
+Bitcoin_Mining.framework.js:2 Renderer: WebKit WebGL
+
+Bitcoin_Mining.framework.js:2 Vendor:   WebKit
+
+Bitcoin_Mining.framework.js:2 Version:  OpenGL ES 3.0 (WebGL 2.0 (OpenGL ES 3.0 Chromium))
+
+Bitcoin_Mining.framework.js:2 GLES:     3
+
+Bitcoin_Mining.framework.js:2  EXT_color_buffer_float GL_EXT_color_buffer_float EXT_color_buffer_half_float GL_EXT_color_buffer_half_float EXT_disjoint_timer_query_webgl2 GL_EXT_disjoint_timer_query_webgl2 EXT_float_blend GL_EXT_float_blend EXT_texture_compression_bptc GL_EXT_texture_compression_bptc EXT_texture_compression_rgtc GL_EXT_texture_compression_rgtc EXT_texture_filter_anisotropic GL_EXT_texture_filter_anisotropic EXT_texture_norm16 GL_EXT_texture_norm16 KHR_parallel_shader_compile GL_KHR_parallel_shader_compile OES_draw_buffers_indexed GL_OES_draw_buffers_indexed OES_texture_float_linear GL_OES_texture_float_linear WEBGL_compressed_texture_s3tc GL_WEBGL_compressed_texture_s3tc WEBGL_compressed_texture_s3tc_srgb GL_WEBGL_compressed_texture_s3tc_srgb WEBGL_debug_renderer_info GL_WEBGL_debug_renderer_info WEBGL_debug_shaders GL_WEBGL_debug_shaders WEBGL_lose_context GL_WEBGL_lose_context WEBGL_multi_draw GL_WEBGL_multi_draw OVR_multiview2 GL_OVR_multiview2
+
+Bitcoin_Mining.framework.js:2 OPENGL LOG: Creating OpenGL ES 3.0 graphics device ; Context level  <OpenGL ES 3.0> ; Context handle 24668616
+
+29The AudioContext was not allowed to start. It must be resumed (or created) after a user gesture on the page.
+Bitcoin_Mining.framework.js:2 Input Manager initialize...
+
+Bitcoin_Mining.framework.js:2 UnloadTime: 0.500000 ms
+
+Bitcoin_Mining.framework.js:2 Ready
+
